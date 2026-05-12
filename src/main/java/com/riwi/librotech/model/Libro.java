484c0cec43c51dbec131ac8f6643e3b7e2ed2adb
LuisCampillo19 <lgcampillo19@gmail.com>
2026-05-12 07:47:51 -0500

    feat: new laboratory number three with used JPA and Hibernate configuratin environment

--- src/main/java/com/riwi/librotech/model/Libro.java
+++ src/main/java/com/riwi/librotech/model/Libro.java
@@ -1,5 +1,11 @@
 package com.riwi.librotech.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +13,23 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "libros")
 public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 150)
     private String tittle;
+
+    @Column(nullable = false, length = 150)
     private String author;
+
+    @Column(unique = true, length = 20)
     private String isbn;
+
+    @Column(name = "publication_year")
     private Integer publicationYear;
 }
