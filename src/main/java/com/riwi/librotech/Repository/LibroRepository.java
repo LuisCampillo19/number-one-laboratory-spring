16a25264354025a44a0dc17ab22bd95c85683cc7
LuisCampillo19 <lgcampillo19@gmail.com>
2026-05-12 07:57:03 -0500

    feat: add interface repository with findByAutor

---
+++ src/main/java/com/riwi/librotech/Repository/LibroRepository.java
@@ -0,0 +1,12 @@
+package com.riwi.librotech.Repository;
+
+import com.riwi.librotech.model.Libro;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+    List<Libro> findByAutor(String autor);
+
+}
