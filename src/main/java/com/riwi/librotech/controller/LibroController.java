39387346eb0dcaeb4d4cc9f7f658fadfce6d9ed9
LuisCampillo19 <lgcampillo19@gmail.com>
2026-05-12 08:54:18 -0500

    feat: update module Controller with Services

--- src/main/java/com/riwi/librotech/controller/LibroController.java
+++ src/main/java/com/riwi/librotech/controller/LibroController.java
@@ -1,11 +1,13 @@
 package com.riwi.librotech.controller;
 
+import com.riwi.librotech.Service.LibroService;
 import com.riwi.librotech.model.Libro;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,13 +18,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/libros")
 public class LibroController {
-    private List<Libro> libros = new ArrayList<>();
-    private Long nextId = 1L;
+    @Autowired
+    private LibroService libroService; // Inyección de dependencias
 
     /**
      * GET /api/libros
@@ -30,7 +33,7 @@ public class LibroController {
      */
     @GetMapping
     public ResponseEntity<List<Libro>> listarLibros() {
-        return ResponseEntity.ok(libros); // return code 200 OK
+        return ResponseEntity.ok(libroService.obtenerTodos()); // return code 200 OK
     }
 
     /**
@@ -39,13 +42,9 @@ public class LibroController {
      */
     @GetMapping("/{id}")
     public ResponseEntity<Libro> obteberLibro(@PathVariable long id) {
-        Optional<Libro> libro = buscarPorId(id);
-
-        if (libro.isPresent()) {
-            return ResponseEntity.ok(libro.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return libroService.obtenerPorId(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
     }
 
     /**
@@ -54,68 +53,30 @@ public class LibroController {
      */
     @PostMapping
     public ResponseEntity<Libro> crearLibro(@RequestBody Libro libro) {
-        libro.setId(nextId++);
-        libros.add(libro);
-        return ResponseEntity.status(HttpStatus.CREATED).body(libro);
+        return ResponseEntity.status(HttpStatus.CREATED).body(libroService.guardar(libro));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Libro> actualizarLibro(@PathVariable Long id, @RequestBody Libro libroActualizado) {
-        Optional<Libro> libroExistente = buscarPorId(id);
-
-        if (libroExistente.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        Libro libro = libroExistente.get();
-        libro.setTittle(libroActualizado.getTittle());
-        libro.setAuthor(libroActualizado.getAuthor());
-        libro.setIsbn(libroActualizado.getIsbn());
-        libro.setPublicationYear(libroActualizado.getPublicationYear());
-
-        return ResponseEntity.ok(libro);
+    public ResponseEntity<Libro> actualizar(@PathVariable Long id, @RequestBody Libro libro) {
+        return ResponseEntity.ok(libroService.actualizar(id, libro));
     }
 
     /**
      * PATCH /api/libros/{id}
      */
     @PatchMapping("/{id}")
-    public ResponseEntity<Libro> actualizarParcial(@PathVariable Long id,
-                                                    @RequestBody Libro cambios) {
-        Optional<Libro> libroExistente = buscarPorId(id);
-
-        if (libroExistente.isEmpty()) {
-            return ResponseEntity.notFound().build(); // 404 Not Found
-        }
-
-        Libro libro = libroExistente.get();
-
-        // Solo actualizamos si el campo viene con valor (no null)
-        if (cambios.getTittle() != null)          libro.setTittle(cambios.getTittle());
-        if (cambios.getAuthor() != null)           libro.setAuthor(cambios.getAuthor());
-        if (cambios.getIsbn() != null)            libro.setIsbn(cambios.getIsbn());
-        if (cambios.getPublicationYear() != 0)    libro.setPublicationYear(cambios.getPublicationYear());
-
-        return ResponseEntity.ok(libro); // 200 OK
+    public ResponseEntity<Libro> actualizarParcial(@PathVariable Long id, @RequestBody Libro cambios) {
+        return ResponseEntity.ok(libroService.actualizarParcial(id, cambios));
     }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarLibro(@PathVariable Long id) {
-        Optional<Libro> libro = buscarPorId(id);
-
-        if (libro.isEmpty()) {
-            return ResponseEntity.notFound().build(); // 404 Not Found
-        }
-
-        libros.remove(libro.get());
-        return ResponseEntity.noContent().build(); // 204 No Content
+        libroService.eliminar(id);
+        return ResponseEntity.noContent().build();
     }
 
-    /**
-    * Method aux
-    */
-    private Optional<Libro> buscarPorId(Long id) {
-        return libros.stream()
-                     .filter(l -> l.getId().equals(id))
-                     .findFirst();
+    @GetMapping("/buscar")
+    private ResponseEntity<List<Libro>> buscarPorAutor(@RequestParam String autor) {
+        return ResponseEntity.ok(libroService.buscarPorAutor(autor));
     }
 }
